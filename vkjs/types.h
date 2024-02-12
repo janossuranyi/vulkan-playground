efec735dc788ebc301002f7c11609f5ab7b08c3e
suranyi.janos <suranyi.janos@eszfk.hu>
2024-02-12 16:45:12 +0100

    .

--- vkjs/types.h
+++ vkjs/types.h
@@ -14,53 +14,5 @@ namespace vkjs {
 	typedef uint8_t		u8;
 	typedef int8_t		s8;
 	typedef uint32_t	uint;
-
-	template<int L, typename T> struct vec;
-
-	template<typename T>
-	struct vec<2, T> {
-		union {
-			struct { T x, y; };
-			struct { T r, g; };
-			struct { T s, t; };
-		};
-		constexpr vec();
-		constexpr vec(T _x, T _y);
-		constexpr vec(const vec&);
-		constexpr size_t length() const { return 2; }
-		constexpr T operator[](size_t i) const;
-		constexpr T& operator[](size_t i);
-		const T& operator[](size_t i) const;
-	};
-
-
-	template<typename T> 
-	constexpr vec<2, T>::vec() { x = y = static_cast<T>(0);  }
-	template<typename T>
-	constexpr vec<2, T>::vec(T _x, T _y) : x(_x), y(_y) {}
-	template<typename T>
-	constexpr vec<2, T>::vec(const vec<2, T>& o) : x(o.x), y(o.y) {}
-	template<typename T>
-	constexpr T vec<2, T>::operator[](size_t i) const {
-		switch (i) {
-		case 0: return x;
-		case 1: return y;
-		default:
-			return static_cast<T>(0);
-		}
-	}
-	template<typename T>
-	constexpr T& vec<2, T>::operator[](size_t i) {
-		switch (i) {
-		case 0: return x;
-		case 1: return y;
-		default:
-			assert(false);
-		}
-	}
-	template<typename T>
-	const T& vec<2, T>::operator[](size_t i) const {
-		return operator[](i);
-	}
 }
 #endif // !VKJS_TYPES_H_
