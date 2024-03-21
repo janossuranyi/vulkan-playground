73794674dab1d2d4db1e64c6cb098794a7c619d6
suranyi.janos <suranyi.janos@eszfk.hu>
2024-03-21 17:22:22 +0100

    work

--- src/demo.cpp
+++ src/demo.cpp
@@ -5,34 +5,29 @@
 
 //#include "jsrlib/jsr_jobsystem2.h"
 
-template<class T, class R = T>
-class cFunc3 {
-public:
-    virtual R operator()(T a, T b, T c) = 0;
+template<class R, class ...Args>
+struct Function
+{
+    virtual R operator()(Args... args) = 0;
 };
 
-template<class T, class R = T>
-class muladd : public cFunc3<T,R> {
-    R operator()(T a, T b, T c) { 
-        T p1 = static_cast<T>(a);
-        T p2 = static_cast<T>(b);
-        T p3 = static_cast<T>(c);
-        return static_cast<R>(p1 * p2 + p3);
+struct MyFunc : public Function<float, int, int, int>
+{
+    float operator()(int a, int b, int c) override
+    {
+        return static_cast<float>(a + b * c);
     }
 };
 
-float func1(float x, cFunc3<float>& fn) {
-    return x * fn(x, 2, 3);
+float foo(int a, int b, int c, Function<float, int, int, int>& fn)
+{
+    return fn(a, b, c) / 0.5f;
 }
 
 void demo()
 {
     jsrlib::gLogWriter.SetFileName("vulkan_engine.log");
 
-    auto* fn1 = new muladd<float>();
-    float x = func1(2.4f, *fn1);
-
-    std::cout << "x=" << x << "\n";
 
     Sample1App* app = new Sample1App(true);
     app->settings.hdr = false;
@@ -46,6 +41,9 @@ void demo()
     
     if (app->init())
     {
+        MyFunc fn;
+
+        std::cout << "fn()=" << foo(2,3,5,fn) << "\n";
         app->prepare();
         app->run();
     }
